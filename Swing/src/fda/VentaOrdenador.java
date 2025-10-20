0d4b5df5188dfe55b902815f3c91a68dc17cf063
alumno <ayoubbenjaddi5@gmail.com>
2025-10-20 09:37:22 +0200

    VentaOrdenador

--- Swing/src/fda/VentaOrdenador.java
+++ Swing/src/fda/VentaOrdenador.java
@@ -14,9 +14,14 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JTextField;
 import java.awt.Font;
-import javax.swing.JRadioButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class VentaOrdenador extends JFrame {
+import javax.swing.JRadioButton;
+import javax.swing.JCheckBox;
+import javax.swing.JButton;
+
+public class VentaOrdenador extends JFrame implements ActionListener{
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
@@ -30,7 +35,7 @@ public class VentaOrdenador extends JFrame {
 	private JLabel monitor;
 	private JLabel discoDuro;
 	private JLabel opciones;
-	private ButtonGroup bg1;
+	private ButtonGroup bg1,bg2,bg3,bg4;
 	private JRadioButton botonMemoria1;
 	private JRadioButton botonMemoria2;
 	private JRadioButton botonMemoria3;
@@ -44,10 +49,12 @@ public class VentaOrdenador extends JFrame {
 	private JRadioButton botonDisco3;
 	private JRadioButton botonDisco4;
 	
+	private JButton añadir,salir;
+	
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -120,12 +127,12 @@ public class VentaOrdenador extends JFrame {
 		
 		discoDuro = new JLabel("Disco Duro");
 		discoDuro.setFont(new Font("Tahoma", Font.BOLD, 11));
-		discoDuro.setBounds(371, 103, 76, 14);
+		discoDuro.setBounds(316, 103, 76, 14);
 		contentPane.add(discoDuro);
 		
 		opciones = new JLabel("Opciones");
 		opciones.setFont(new Font("Tahoma", Font.BOLD, 11));
-		opciones.setBounds(496, 103, 76, 14);
+		opciones.setBounds(434, 103, 76, 14);
 		contentPane.add(opciones);
 		
 		//Botones 
@@ -152,6 +159,8 @@ public class VentaOrdenador extends JFrame {
 		bg1.add(botonProcesador3);
 		bg1.add(botonProcesador4);
 		
+		//grupo2 de memoria
+		bg2=new ButtonGroup();
 		botonMemoria1 = new JRadioButton("128 Mb");
 		botonMemoria1.setBounds(124, 124, 100, 23);
 		contentPane.add(botonMemoria1);
@@ -168,41 +177,116 @@ public class VentaOrdenador extends JFrame {
 		botonMemoria4.setBounds(124, 202, 98, 23);
 		contentPane.add(botonMemoria4);
 		
-		botonMonitor1 = new JRadioButton("New radio button");
-		botonMonitor1.setBounds(225, 124, 109, 23);
+		bg2.add(botonMemoria1);
+		bg2.add(botonMemoria2);
+		bg2.add(botonMemoria3);
+		bg2.add(botonMemoria4);
+
+		//grupo3 monitor
+		bg3=new ButtonGroup();
+
+		botonMonitor1 = new JRadioButton("15\"");
+		botonMonitor1.setBounds(225, 124, 76, 23);
 		contentPane.add(botonMonitor1);
 		
-		botonMonitor2 = new JRadioButton("New radio button");
-		botonMonitor2.setBounds(225, 150, 109, 23);
+		botonMonitor2 = new JRadioButton("17\"");
+		botonMonitor2.setBounds(225, 150, 76, 23);
 		contentPane.add(botonMonitor2);
 		
-		botonMonitor3 = new JRadioButton("New radio button");
-		botonMonitor3.setBounds(225, 176, 109, 23);
+		botonMonitor3 = new JRadioButton("TFT 15\"");
+		botonMonitor3.setBounds(225, 176, 76, 23);
 		contentPane.add(botonMonitor3);
 		
-		botonMonitor4 = new JRadioButton("New radio button");
-		botonMonitor4.setBounds(225, 202, 109, 23);
+		botonMonitor4 = new JRadioButton("TFT 17\"");
+		botonMonitor4.setBounds(225, 202, 76, 23);
 		contentPane.add(botonMonitor4);
 		
-		botonDisco1 = new JRadioButton("New radio button");
-		botonDisco1.setBounds(359, 124, 109, 23);
+		bg3.add(botonMonitor1);
+		bg3.add(botonMonitor2);
+		bg3.add(botonMonitor3);
+		bg3.add(botonMonitor4);
+
+		
+		//grupo4 disco 
+		bg4=new ButtonGroup();
+
+		botonDisco1 = new JRadioButton("60 Gb");
+		botonDisco1.setBounds(316, 124, 85, 23);
 		contentPane.add(botonDisco1);
 		
-		botonDisco2 = new JRadioButton("New radio button");
-		botonDisco2.setBounds(359, 150, 109, 23);
+		botonDisco2 = new JRadioButton("80  Gb");
+		botonDisco2.setBounds(316, 150, 76, 23);
 		contentPane.add(botonDisco2);
 		
-		botonDisco3 = new JRadioButton("New radio button");
-		botonDisco3.setBounds(359, 176, 109, 23);
+		botonDisco3 = new JRadioButton("120 Gb");
+		botonDisco3.setBounds(316, 176, 64, 23);
 		contentPane.add(botonDisco3);
 		
-		botonDisco4 = new JRadioButton("New radio button");
-		botonDisco4.setBounds(359, 202, 109, 23);
+		botonDisco4 = new JRadioButton("200 Gb");
+		botonDisco4.setBounds(316, 202, 85, 23);
 		contentPane.add(botonDisco4);
 		
+		bg4.add(botonDisco1);
+		bg4.add(botonDisco2);
+		bg4.add(botonDisco3);
+		bg4.add(botonDisco4);
+		
+		
+		//opciones check
+		JCheckBox opciones1 = new JCheckBox("Grabadora DVD");
+		opciones1.setBounds(417, 124, 138, 23);
+		contentPane.add(opciones1);
+		
+		JCheckBox opciones2 = new JCheckBox("Wifi");
+		opciones2.setBounds(417, 150, 138, 23);
+		contentPane.add(opciones2);
+		
+		JCheckBox opciones3 = new JCheckBox("Sintonizador TV");
+		opciones3.setBounds(417, 176, 138, 23);
+		contentPane.add(opciones3);
+		
+		JCheckBox opciones4 = new JCheckBox("Backup/Restore");
+		opciones4.setBounds(417, 202, 116, 23);
+		contentPane.add(opciones4);
+		
+		JButton añadir = new JButton("Añadir");
+		añadir.setBounds(10, 269, 89, 23);
+		contentPane.add(añadir);
+		
+		JButton buscar = new JButton("Buscar");
+		buscar.setBounds(114, 269, 89, 23);
+		contentPane.add(buscar);
+		
+		JButton eliminar = new JButton("Eliminar");
+		eliminar.setBounds(212, 269, 89, 23);
+		contentPane.add(eliminar);
+		
+		JButton cancelar = new JButton("Cancelar");
+		cancelar.setBounds(485, 249, 89, 23);
+		contentPane.add(cancelar);
+		
+		JButton salir = new JButton("Salir");
+		salir.setBounds(485, 283, 89, 23);
+		contentPane.add(salir);
+		
+		
+		añadir.addActionListener(this);
+		salir.addActionListener(this);
 		
 		
 		
 		
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==añadir) {
+			
+			listaClientes.setText(cliente.getText());
+		}
+		if(e.getSource()==salir) {
+			System.exit(0);
+		}
 	}
 }
